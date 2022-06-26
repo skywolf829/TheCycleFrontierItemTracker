9e02a4fa7b1aa1ec8674e8f09a86baef2239be07
Connor Flanigan <connorflanigan@gmail.com>
2022-06-26 19:52:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,130 +8,123 @@ from bs4 import BeautifulSoup
 import requests
 
 
-global items_list
-items_list = None
+# global items_list
+# items_list = None
 
 
-def log_visitor():
-    visitor_ip = request.remote_addr
-    visitor_requested_path = request.full_path
-    now = datetime.now()
-    dt = now.strftime("%d/%m/%Y %H:%M:%S")
+class CycleApp:
+    def __init__(self):
+        self.items_list = None
+        self.app = Flask(__name__)
+        self.app.route('/')(self.index)
+        self.app.route('/index')(self.index)
+        self.app.run(host='0.0.0.0', debug=False, port=8080)
 
-    pth = os.path.dirname(os.path.abspath(__file__))
-    f = open(os.path.join(pth, "log.txt"), "a")
-    f.write(dt + ": " + str(visitor_ip) + " " + str(visitor_requested_path) + "\n")
-    f.close()
+    @staticmethod
+    def log_visitor():
+        visitor_ip = request.remote_addr
+        visitor_requested_path = request.full_path
+        now = datetime.now()
+        dt = now.strftime("%d/%m/%Y %H:%M:%S")
 
+        pth = os.path.dirname(os.path.abspath(__file__))
+        f = open(os.path.join(pth, "log.txt"), "a")
+        f.write(dt + ": " + str(visitor_ip) + " " + str(visitor_requested_path) + "\n")
+        f.close()
 
-def index():
-    global items_list
-    log_visitor()
-    if items_list is None:
-        # download_images();
-        populate_items()
+    def index(self):
+        self.log_visitor()
+        if self.items_list is None:
+            self.populate_items()
 
-    return render_template('index.html', items_list=items_list)
+        return render_template('index.html', items_list=self.items_list)
 
+    @staticmethod
+    def download_images():
+        url = "https://thecyclefrontier.fandom.com/wiki/Loot"
+        page = requests.get(url)
 
-def download_images():
-    url = "https://thecyclefrontier.fandom.com/wiki/Loot"
-    page = requests.get(url)
+        soup = BeautifulSoup(page.text)
+        rows = soup.find("div", {"id": "content"}).find("tbody").find_all("tr")
 
-    soup = BeautifulSoup(page.text)
-    rows = soup.find("div", {"id": "content"}).find("tbody").find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
 
-    for row in rows:
-        cells = row.find_all("td")
-
-        if len(cells) > 0:
-            if cells[1].string is None:
-                if cells[1].find("span") is not None:
-                    name = cells[1].find("span").string.strip()
-                elif cells[1].find("a") is not None:
-                    name = cells[1].find("a").string.strip()
+            if len(cells) > 0:
+                if cells[1].string is None:
+                    if cells[1].find("span") is not None:
+                        name = cells[1].find("span").string.strip()
+                    elif cells[1].find("a") is not None:
+                        name = cells[1].find("a").string.strip()
+                    else:
+                        raise Exception("an issue with cells occurred, {}".format(cells[1]))
                 else:
-                    raise Exception("an issue with cells occurred, {}".format(cells[1]))
-            else:
-                name = cells[1].string.strip()
+                    name = cells[1].string.strip()
 
-            if cells[0].find("img") is not None:
-                print(cells[0])
-                image_src = cells[0].find("a")['href']
-                img = requests.get(image_src, stream=True)
-                with open(os.path.join(os.getcwd(), "static", "img",
-                                       name + ".png"), 'wb') as f:
-                    img.raw.decode_content = True
-                    shutil.copyfileobj(img.raw, f)
+                if cells[0].find("img") is not None:
+                    print(cells[0])
+                    image_src = cells[0].find("a")['href']
+                    img = requests.get(image_src, stream=True)
+                    with open(os.path.join(os.getcwd(), "static", "img",
+                                           name + ".png"), 'wb') as f:
+                        img.raw.decode_content = True
+                        shutil.copyfileobj(img.raw, f)
 
+    def populate_items(self):
+        self.items_list = []
+        url = "https://thecyclefrontier.fandom.com/wiki/Loot"
+        browser = mechanicalsoup.Browser()
+        page = browser.get(url)
 
-def populate_items():
-    global items_list
-    items_list = []
-    url = "https://thecyclefrontier.fandom.com/wiki/Loot"
-    browser = mechanicalsoup.Browser()
-    page = browser.get(url)
+        soup = page.soup
+        rows = soup.find("div", {"id": "content"}).find("tbody").find_all("tr")
 
-    soup = page.soup
-    rows = soup.find("div", {"id": "content"}).find("tbody").find_all("tr")
+        item_no = 0
+        for row in rows:
+            cells = row.find_all("td")
+            name = ""
+            link = ""
+            image_src = ""
+            weight = ""
+            sell_price = ""
+            price_per_weight = ""
 
-    item_no = 0
-    for row in rows:
-        cells = row.find_all("td")
-        name = ""
-        link = ""
-        image_src = ""
-        weight = ""
-        sell_price = ""
-        price_per_weight = ""
+            if len(cells) > 0:
+                if cells[1].string is None:
+                    if cells[1].find("span") is not None:
+                        name = cells[1].find("span").string.strip()
+                    elif cells[1].find("a") is not None:
+                        name = cells[1].find("a").string.strip()
+                        link = cells[1].find("a", href=True)['href']
+                        link = link.replace("/wiki",
+                                            "https://thecyclefrontier.fandom.com/wiki")
+                else:
+                    name = cells[1].string.strip()
 
-        if len(cells) > 0:
-            if cells[1].string is None:
-                if cells[1].find("span") is not None:
-                    name = cells[1].find("span").string.strip()
-                elif cells[1].find("a") is not None:
-                    name = cells[1].find("a").string.strip()
-                    link = cells[1].find("a", href=True)['href']
-                    link = link.replace("/wiki",
-                                        "https://thecyclefrontier.fandom.com/wiki")
-            else:
-                name = cells[1].string.strip()
+                if cells[0].find("img") is not None:
+                    image_src = "img/" + name + ".png"
 
-            if cells[0].find("img") is not None:
-                image_src = "img/" + name + ".png"
+                if cells[2].string is not None:
+                    weight = cells[2].string.strip()
 
-            if cells[2].string is not None:
-                weight = cells[2].string.strip()
+                if cells[3].string is not None:
+                    sell_price = cells[3].string.strip()
 
-            if cells[3].string is not None:
-                sell_price = cells[3].string.strip()
+                if cells[4].string is not None:
+                    price_per_weight = cells[4].string.strip()
 
-            if cells[4].string is not None:
-                price_per_weight = cells[4].string.strip()
-
-            item = {
-                "id": str(item_no),
-                "name": name,
-                "link": link,
-                "image_src": image_src,
-                "weight": weight,
-                "price_per_weight": price_per_weight,
-                "sell_price": sell_price
-            }
-            items_list.append(item)
-            item_no += 1
-
-
-def initialize_app(app: Flask):
-    app.route('/')(index)
-    app.route('/index')(index)
-
-
-def main():
-    app = Flask(__name__)
-    initialize_app(app)
-    app.run(host='0.0.0.0', debug=False, port=8080)
+                item = {
+                    "id": str(item_no),
+                    "name": name,
+                    "link": link,
+                    "image_src": image_src,
+                    "weight": weight,
+                    "price_per_weight": price_per_weight,
+                    "sell_price": sell_price
+                }
+                self.items_list.append(item)
+                item_no += 1
 
 
 if __name__ == '__main__':
-    main()
+    CycleApp()
